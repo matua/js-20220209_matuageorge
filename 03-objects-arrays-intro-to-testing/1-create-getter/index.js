1704ec97f134aebbe9ee3be8dc79d75d6dd8c573
Georgii <matuageorge@gmail.com>
2022-02-19 13:19:22 +0300

    Task 3 - create-getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const args = path.split('.');
+  return function (obj) {
+    for (let a of args) {
+      let objElement = obj[a];
+      if (objElement === undefined) {
+        return undefined;
+      }
+      obj = objElement;
+    }
+    return obj;
+  };
 }
