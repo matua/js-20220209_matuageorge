606838a05089bdc1a1a1cab897261dde2654bb17
Georgii <matuageorge@gmail.com>
2022-02-19 18:51:31 +0300

    Task 3 - trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,32 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  } else if (size === undefined) {
+    return string;
+  }
 
+  let out = '';
+
+  for (let i = 0, sl = string.length; i < sl; i++) {
+
+    let ch = string[i];
+    let buf = ch;
+
+    for (++i; i < sl; i++) {
+
+      if (string[i] !== ch) {
+        --i;
+        break;
+      }
+
+      if (buf.length < size) {
+        buf += ch;
+      }
+
+    }
+    out += buf;
+  }
+  return out;
 }
